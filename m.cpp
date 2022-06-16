c720658a754cf691f585f6818b4d3f97d8fa79fd
joe <ngugijoseph992@gmail.com>
2022-06-16 23:18:45 +0200

    cpp updates

---
+++ m.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    std::cout << "Hello World!\n";
+
+    for (int i = 0; i<=100; i++){
+        std:: cout << i << "\n";
+    }
+
+    return 0;
+}
+
---
+++ user_input.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <chrono>
+
+using namespace std;
+
+int main(){
+    string first_name, last_name, date_of_birth;
+    std::cout << "Please enter your first name!\n";
+    std::cin >> first_name;
+    std::cout << "Please enter your Last name!\n";
+    std::cin >> last_name;
+    std::cout << "Please enter your date of birth (DD/MM/YY)!\n";
+    std::cin >> date_of_birth;
+
+    //std:cout << first_name <<  last_name << date_of_birth << "\n";
+    printf("First Name: %s\nLast Name: %s\nDOB: %s\n",first_name.c_str(),last_name.c_str(),date_of_birth.c_str());
+}
