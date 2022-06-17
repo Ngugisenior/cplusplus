9804e4b2261bcac3ff9fd96493332aa61cf0bf16
joe <ngugijoseph992@gmail.com>
2022-06-17 14:50:59 +0200

    cpp updates

--- curl_test.c
+++ curl_test.c
@@ -8,6 +8,10 @@ int main (){
     b). set options for transfer
     c). perform transfer
     d). cleanup after transfer
+
+    NOTE:
+    - to run the program we must link the libcurl library as shown below
+      g++ -L/usr/lib/x86_64-linux-gnu  curl_test.c -o curltest -lcurl
     */
 
     printf("Hello World!\n");
@@ -18,6 +22,7 @@ int main (){
     curl = curl_easy_init();
 
     if(curl){
+        /* Curl a webpage */
         curl_easy_setopt(curl, CURLOPT_URL, "http://localhost:80");
 
 
