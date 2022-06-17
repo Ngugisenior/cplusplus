2e4bb56dbc3cd13a5ab2b08fd319b5e12b80f1d8
joe <ngugijoseph992@gmail.com>
2022-06-17 14:48:52 +0200

    cpp updates

--- curl_test.c
+++ curl_test.c
@@ -18,7 +18,7 @@ int main (){
     curl = curl_easy_init();
 
     if(curl){
-        curl_easy_setopt(curl, CURLOPT_URL, "http:localhost:80");
+        curl_easy_setopt(curl, CURLOPT_URL, "http://localhost:80");
 
 
         res=curl_easy_perform(curl);
